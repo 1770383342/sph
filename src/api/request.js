c5ff7242f229b01c1872fcf1f2aa4c1025f27c48
AYJL <1770383342@qq.com>
2023-05-11 22:13:28 +0800

    引入接口进度条

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,10 @@
 // 对于axios进行二次封装
 import axios from "axios";
+// 引入进度条
+import nprogress from "nprogress";
+// 引入进度条样式
+import "nprogress/nprogress.css"
+//  start：基督徒开始 done：进度条结束
 
 // 1:利用axios对象的方法create，去创建一个axios实例
 // 2:request就是axios，只不过稍微配置了一下
@@ -14,12 +19,15 @@ const request = axios.create({
 // 请求拦截器：在发送请求之前，请求拦截器可以监测到，可以在请求发出去之前做一些事情
 request.interceptors.request.use((config) => {
     // config:配置对象，对象里面有一个属性很重要，header请求头
+    // 进度条开始动
+    nprogress.start()
     return config
 })
 
 // 响应拦截器
 request.interceptors.response.use((res) => {
     // 成功的回调函数：服务器响应数据回来以后，响应拦截器可以检测到，可以做一些事情
+    nprogress.done()
     return res.data
 }, (error) => {
     // 服务器响应失败的回调函数

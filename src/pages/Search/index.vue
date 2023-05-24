e1253532adbc6f1508f51ec14c6af56ddc1d36bc
“Home_pc” <1770383342@qq.com>
2023-05-24 19:52:48 +0800

    图片懒加载

--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -70,7 +70,7 @@
                   <div class="p-img">
                     <!-- 路由跳转记得带参数 -->
                     <router-link :to="`/detail/${good.id}`">
-                      <img :src="good.defaultImg" />
+                      <img v-lazy="good.defaultImg" />
                     </router-link>
                   </div>
                   <div class="price">
